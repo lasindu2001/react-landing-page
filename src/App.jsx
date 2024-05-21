59c937f67116c2428ea78efdacd1c12df4931d54
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-05-21 13:57:04 +0530

    create button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-function App() {
+import ButtonGradient from './assets/svg/ButtonGradient'
+import Button from './components/Button'
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+        <Button className="mt-10" href="#login">
+          Something
+        </Button>
+      </div>
+      <ButtonGradient />
+    </>
   )
 }
 
